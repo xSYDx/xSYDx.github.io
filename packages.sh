1d9e0869bac221a36117f56606e8d7403866c6db
xsydx <sidskyl1n3@gmail.com>
2017-01-31 16:31:46 -0500

    fix?

--- packages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dpkg-deb -bZgzip projects/classicuniversalforce debs
-dpkg-deb -bZgzip projects/kuaidial debs
-#re pack all debs.
--- push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dpkg-scanpackages ./debs > Packages
-bzip2 -fks Packages
-#refresh all date.
